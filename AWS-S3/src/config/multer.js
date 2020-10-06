a1cbe9beed8ba0f9806726b6a2b823546a6a7f7a
Gabriel Silva <gabriel.etefmc@gmail.com>
2020-10-06 20:47:40 -0300

    S3

--- AWS-S3/src/config/multer.js
+++ AWS-S3/src/config/multer.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 
+
 const storageTypes = {
   local: multer.diskStorage({
     destination: (req, file, cb) => {
